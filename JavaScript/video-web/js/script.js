2ce7642a5812338bc26fdbc06e7012f54010204b
11111111u <72383958+11111111u@users.noreply.github.com>
2022-12-27 00:38:38 +0200

    v.3

---
+++ JavaScript/video-web/js/script.js
@@ -0,0 +1,15 @@
+var video = document.getElementById("video-1");
+video.autoplay = true;
+video.load();
+video.loop = true;
+
+
+// var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+// if (!isChrome){
+//     $('#iframeAudio').remove()
+// }
+// else {
+//     $('#playAudio').remove() // just to make sure that it will not have 2x audio in the background 
+// }
+
+
